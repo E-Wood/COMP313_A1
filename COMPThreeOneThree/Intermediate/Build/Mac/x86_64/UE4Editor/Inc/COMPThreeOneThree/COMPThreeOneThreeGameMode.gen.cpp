11fa19cdec6fe21ba72c13e5bdad35240c0a4a63
Guest User <Guest@WG401-F-06-06.mddn.vuw.ac.nz>
2021-07-19 13:47:08 +1200

    Started template project - added disappearing platform actors

---
+++ COMPThreeOneThree/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/COMPThreeOneThree/COMPThreeOneThreeGameMode.gen.cpp
@@ -0,0 +1,86 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "COMPThreeOneThree/COMPThreeOneThreeGameMode.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeCOMPThreeOneThreeGameMode() {}
+// Cross Module References
+	COMPTHREEONETHREE_API UClass* Z_Construct_UClass_ACOMPThreeOneThreeGameMode_NoRegister();
+	COMPTHREEONETHREE_API UClass* Z_Construct_UClass_ACOMPThreeOneThreeGameMode();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_COMPThreeOneThree();
+// End Cross Module References
+	void ACOMPThreeOneThreeGameMode::StaticRegisterNativesACOMPThreeOneThreeGameMode()
+	{
+	}
+	UClass* Z_Construct_UClass_ACOMPThreeOneThreeGameMode_NoRegister()
+	{
+		return ACOMPThreeOneThreeGameMode::StaticClass();
+	}
+	struct Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_COMPThreeOneThree,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "COMPThreeOneThreeGameMode.h" },
+		{ "ModuleRelativePath", "COMPThreeOneThreeGameMode.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ACOMPThreeOneThreeGameMode>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::ClassParams = {
+		&ACOMPThreeOneThreeGameMode::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x008802ACu,
+		METADATA_PARAMS(Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_ACOMPThreeOneThreeGameMode()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_ACOMPThreeOneThreeGameMode_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ACOMPThreeOneThreeGameMode, 3042375054);
+	template<> COMPTHREEONETHREE_API UClass* StaticClass<ACOMPThreeOneThreeGameMode>()
+	{
+		return ACOMPThreeOneThreeGameMode::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ACOMPThreeOneThreeGameMode(Z_Construct_UClass_ACOMPThreeOneThreeGameMode, &ACOMPThreeOneThreeGameMode::StaticClass, TEXT("/Script/COMPThreeOneThree"), TEXT("ACOMPThreeOneThreeGameMode"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ACOMPThreeOneThreeGameMode);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ COMPThreeOneThree/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/COMPThreeOneThree/COMPThreeOneThreeProjectile.generated.h
@@ -0,0 +1,113 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class UPrimitiveComponent;
+class AActor;
+struct FVector;
+struct FHitResult;
+#ifdef COMPTHREEONETHREE_COMPThreeOneThreeProjectile_generated_h
+#error "COMPThreeOneThreeProjectile.generated.h already included, missing '#pragma once' in COMPThreeOneThreeProjectile.h"
+#endif
+#define COMPTHREEONETHREE_COMPThreeOneThreeProjectile_generated_h
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_SPARSE_DATA
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execOnHit);
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execOnHit);
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACOMPThreeOneThreeProjectile(); \
+	friend struct Z_Construct_UClass_ACOMPThreeOneThreeProjectile_Statics; \
+public: \
+	DECLARE_CLASS(ACOMPThreeOneThreeProjectile, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/COMPThreeOneThree"), NO_API) \
+	DECLARE_SERIALIZER(ACOMPThreeOneThreeProjectile) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACOMPThreeOneThreeProjectile(); \
+	friend struct Z_Construct_UClass_ACOMPThreeOneThreeProjectile_Statics; \
+public: \
+	DECLARE_CLASS(ACOMPThreeOneThreeProjectile, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/COMPThreeOneThree"), NO_API) \
+	DECLARE_SERIALIZER(ACOMPThreeOneThreeProjectile) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACOMPThreeOneThreeProjectile(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACOMPThreeOneThreeProjectile) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACOMPThreeOneThreeProjectile); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACOMPThreeOneThreeProjectile); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACOMPThreeOneThreeProjectile(ACOMPThreeOneThreeProjectile&&); \
+	NO_API ACOMPThreeOneThreeProjectile(const ACOMPThreeOneThreeProjectile&); \
+public:
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACOMPThreeOneThreeProjectile(ACOMPThreeOneThreeProjectile&&); \
+	NO_API ACOMPThreeOneThreeProjectile(const ACOMPThreeOneThreeProjectile&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACOMPThreeOneThreeProjectile); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACOMPThreeOneThreeProjectile); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACOMPThreeOneThreeProjectile)
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CollisionComp() { return STRUCT_OFFSET(ACOMPThreeOneThreeProjectile, CollisionComp); } \
+	FORCEINLINE static uint32 __PPO__ProjectileMovement() { return STRUCT_OFFSET(ACOMPThreeOneThreeProjectile, ProjectileMovement); }
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_12_PROLOG
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_PRIVATE_PROPERTY_OFFSET \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_SPARSE_DATA \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_RPC_WRAPPERS \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_INCLASS \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_PRIVATE_PROPERTY_OFFSET \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_SPARSE_DATA \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_INCLASS_NO_PURE_DECLS \
+	COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> COMPTHREEONETHREE_API UClass* StaticClass<class ACOMPThreeOneThreeProjectile>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID COMPThreeOneThree_Source_COMPThreeOneThree_COMPThreeOneThreeProjectile_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
